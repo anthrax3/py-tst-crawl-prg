a8e60af3fd3586db56e4d3653bb23705ff5c9810
oilnur <oilnur@localhost.localdomain>
2018-04-27 11:15:14 +0300

    fix

--- autoreglove/ex_realspeaker.py
+++ autoreglove/ex_realspeaker.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 import time
 import pyautogui
+import os
 
 
 user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101"
@@ -18,8 +19,8 @@ options.add_argument("test-type")
 #options.add_argument('ignore-certificate-errors')
 options.add_argument('window-size=1200x600')
 
-#driver = webdriver.Firefox()
-driver = webdriver.Chrome(chrome_options=options)
+driver = webdriver.Firefox()
+#driver = webdriver.Chrome(chrome_options=options)
 #driver = webdriver.Chrome()
 driver.get('https://www.realspeaker.net/')
 time.sleep(2)
@@ -31,11 +32,12 @@ elem = driver.find_elements_by_css_selector('button[class*="ml-2 mb-3 btn btn--r
 elem[0].click()
 time.sleep(1)
 # указываем путь до файла
-path_dir = ""
+path_dir = os.getcwd()+"/mp3/"
 path_filename = "test.mp3"
-elem = driver.find_elements_by_css_selector('div[id="uploader"] input[type="file"]')
+path_full = path_dir+path_filename
+elem = driver.find_elements_by_css_selector('div input[title="file input"]')
 print(elem)
-elem[0].send_keys("test.mp3")
+elem[0].send_keys(path_full)
 time.sleep(2)
 elem = driver.find_elements_by_css_selector('button[class*="ml-2 mb-3 text--lower btn btn--raised primary"]')
 #print(elem)
@@ -43,7 +45,24 @@ elem[0].click()
 time.sleep(1)
 
 elem = driver.find_elements_by_css_selector('div[class="list__tile__title pt-1"]')
-for el in elem:
-    print(el.text)
+elem_button = driver.find_elements_by_css_selector('div[class="list__tile__action text-xs-right"] button[class="btn btn--raised btn--small primary"]')
+print(len(elem))
+print(len(elem_button))
+for i in range(0, len(elem)):
+    if elem[i].text.strip()==path_filename:
+        elem_button[i].click()
+        break
+time.sleep(5)
 
-#
+elem = driver.find_elements_by_css_selector('div[class="list__tile__title pt-1"]')
+elem_button = driver.find_elements_by_css_selector('div[class="list__tile__action text-xs-right"] button[class="btn btn--raised success"]')
+print(len(elem))
+print(len(elem_button))
+for i in range(0, len(elem)):
+    if elem[i].text.strip()==path_filename:
+        elem_button[i].click()
+        break
+
+
+# преход ко второй странице (списка файлов на транскрипцию)
+
